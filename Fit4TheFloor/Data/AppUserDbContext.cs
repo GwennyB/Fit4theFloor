ea62ca46815deec97b248b66bf8cbd5f53bc83b8
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-05-13 10:01:36 -0700

    scaffolding

---
+++ Fit4TheFloor/Data/AppUserDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Fit4TheFloor.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fit4TheFloor.Data
+{
+    // This class reflects the database context for the 'AppUser' database. It is derived from the Microsoft Identity library for authentication, and build relies on Entity Framework Core (ORM).
+    public class AppUserDbContext : IdentityDbContext<AppUser>
+    {
+        public AppUserDbContext(DbContextOptions<AppUserDbContext> options) : base(options)
+        {
+
+        }
+
+
+
+    }
+}
---
+++ Fit4TheFloor/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fit4TheFloor.Data;
+using Fit4TheFloor.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fit4TheFloor
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppUserDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddDbContext<AppUserDbContext>(options =>
+                options.UseSqlServer($"Data Source={Configuration["RDS_HOSTNAME_USER"]};Initial Catalog={Configuration["RDS_DBNAME_USER"]};User ID={Configuration["RDS_USERNAME_USER"]};Password={Configuration["RDS_PASSWORD_USER"]}"));
+
+            services.AddDbContext<SalesDbContext>(options =>
+                options.UseSqlServer($"Data Source={Configuration["RDS_HOSTNAME_CONTENT"]};Initial Catalog={Configuration["RDS_DBNAME_SALES"]};User ID={Configuration["RDS_USERNAME_CONTENT"]};Password={Configuration["RDS_PASSWORD_CONTENT"]}"));
+
+            services.AddDbContext<StatsDbContext>(options =>
+                options.UseSqlServer($"Data Source={Configuration["RDS_HOSTNAME_CONTENT"]};Initial Catalog={Configuration["RDS_DBNAME_STATS"]};User ID={Configuration["RDS_USERNAME_CONTENT"]};Password={Configuration["RDS_PASSWORD_CONTENT"]}"));
+
+            services.AddMvc();
+
+            //services.AddScoped<ISFAppManager, SFAppMgmtSvc>();
+            //services.AddScoped<IEvaluationManager, EvaluationMgmtSvc>();
+            //services.AddScoped<IAppResponseManager, AppResponseMgmtSvc>();
+            //services.AddScoped<IQuestionManager, QuestionMgmtSvc>();
+            //services.AddScoped<IAppUserManager, AppUserMgmtSvc>();
+
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseStatusCodePagesWithReExecute("/error/{0}");
+
+            app.UseMvc(route =>
+            {
+                route.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                    );
+            });
+        }
+
+
+    }
+}
