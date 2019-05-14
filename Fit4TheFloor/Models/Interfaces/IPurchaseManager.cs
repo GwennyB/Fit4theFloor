c10845cdd10df5011798b53b1678c96e142185b9
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-05-14 11:51:58 -0700

    built PurchaseMgmtSvc

--- Fit4TheFloor/Models/Interfaces/IPurchaseManager.cs
+++ Fit4TheFloor/Models/Interfaces/IPurchaseManager.cs
@@ -7,5 +7,12 @@ namespace Fit4TheFloor.Models.Interfaces
 {
     interface IPurchaseManager
     {
+        Task<Purchase> GetPurchaseAsync(int id);
+        Task<Purchase> GetPurchaseAsync(int cartID, int productID);
+        Task<List<Purchase>> GetAllPurchasesByCartAsync(int cartID);
+        Task<List<Purchase>> GetAllClosedPurchasesByProductAsync(int productID);
+        Task<Purchase> CreatePurchaseAsync(Purchase item);
+        Task<Purchase> UpdatePurchaseAsync(Purchase item);
+        Task<bool> DeletePurchaseAsync(int id);
     }
 }
